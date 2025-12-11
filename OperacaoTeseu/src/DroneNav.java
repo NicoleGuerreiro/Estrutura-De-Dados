04da01b74301cda1e182aadffb54a50d68ba029e
NicoleGuerreiro <nicole.guerreiro09@aluno.ifce.edu.br>
2025-12-11 18:40:02 -0300

    Add files via upload

---
+++ OperacaoTeseu/src/DroneNav.java
@@ -0,0 +1,120 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class DroneNav {
+    private Map<Integer, List<Integer>> adjCams;
+
+
+    public DroneNav(){
+        this.adjCams = new HashMap<>();
+    }
+
+    public void addTunel(int v, int w){
+        adjCams.putIfAbsent(v, new ArrayList<>());
+        adjCams.putIfAbsent(w, new ArrayList<>());
+
+        adjCams.get(v).add(w);
+        adjCams.get(w).add(v);
+    }
+
+    public void loadMine (String filename){
+        try(BufferedReader br = new BufferedReader(new FileReader(filename))){
+            String line = br.readLine();
+            int numCams = Integer.parseInt(line.trim());
+
+            while((line = br.readLine()) != null){
+                String[] parts = line.split(" ");
+                int v = Integer.parseInt(parts[0]);
+                int w = Integer.parseInt(parts[1]);
+                addTunel(v,w);
+            }
+            System.out.println("Mapa carregado");
+        }
+        catch (IOException e){
+            System.err.println("Erro ao ler arquivo: " + e.getMessage());
+        }
+    }
+
+    public List<Integer> getVizinhos(int v){
+        return adjCams.getOrDefault(v, Collections.emptyList());
+    }
+    public Map<Integer, List<Integer>> getAdjCams(){
+        return adjCams;
+    }
+    public void startMappingDFS(int droneStart, Set<Integer> visited) {
+        dfs(droneStart, visited);
+        System.out.println();
+
+        if (visited.size() != adjCams.size()) {
+            System.out.println("Mapeamento incompleto. Áreas inacessíveis detectadas.");
+        }
+    }
+
+    private void dfs(int node, Set<Integer> visited) {
+        if (visited.contains(node)) return;
+
+        visited.add(node);
+        System.out.print(node + " ");
+
+        for (int vizinho : getVizinhos(node)) {
+            if (!visited.contains(vizinho)) {
+                dfs(vizinho, visited);
+            }
+        }
+    }
+
+    public void findShorttestPathBFS(int startNode, int targetNode){
+        System.out.print("Rota de Resgate BFS: ");
+
+        Queue<Integer> fila = new LinkedList<>();
+        boolean[] visited = new boolean[getAdjCams().size()];
+
+        int[] pai = new int[getAdjCams().size()];
+        Arrays.fill(pai, -1);
+
+        fila.add(startNode);
+        visited[startNode] = true;
+        pai[startNode] = -1;
+
+        boolean encontrado = false;
+
+        while(!fila.isEmpty()){
+            int atual = fila.poll();
+
+            if(atual == targetNode){
+                encontrado = true;
+                break;
+            }
+            for (int vizinho : adjCams.get(atual)){
+                if (!visited[vizinho]){
+                    visited[vizinho] = true;
+                    pai[vizinho] = atual;
+                    fila.add(vizinho);
+                }
+            }
+        }
+
+        if (!encontrado){
+            System.out.println("Alvo inalcançável");
+            return;
+        }
+
+        List<Integer> caminho = new ArrayList<>();
+        int atual = targetNode;
+
+        while(atual != -1){
+            caminho.add(atual);
+            atual = pai[atual];
+        }
+
+        Collections.reverse(caminho);
+
+        for (int i = 0; i< caminho.size(); i++){
+            System.out.print(caminho.get(i) + " ");
+        }
+
+        System.out.println();
+    }
+}
---
+++ OperacaoTeseu/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args){
+        DroneNav drone = new DroneNav();
+
+        drone.loadMine("mina.txt");
+
+        System.out.print("Mapeamento DFS (Ordem): ");
+        Set<Integer> visitados = new HashSet<>();
+        drone.startMappingDFS(0, visitados);
+        System.out.println("\n");
+        drone.findShorttestPathBFS(0, 4);
+
+    }
+}
